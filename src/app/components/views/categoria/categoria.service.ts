9d9076bd7604e2668648290f242bc5931dbdc45d
jefersonpenariol <jeferson.penariol@gmail.com>
2021-07-14 18:58:48 -0300

    categoria update

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -36,4 +36,9 @@ export class CategoriaService {
     const url = this.baseUrl + '/categorias/' + id;
     return this.http.delete<void>(url);
   }
+
+  update(categoria: Categoria):Observable<void>{
+    const url = this.baseUrl + '/categorias/' + categoria.id;
+    return this.http.put<void>(url, categoria)
+  }
 }
